d15615bc66aafec4145c4747064f31700522bd74
Yvaine Liew <yvaine8888@gmail.com>
2025-10-21 17:15:45 -0500

    Add product fetching in Featured component
    Fetch and set featured products from API on component mount.

--- client/src/components/featured.jsx
+++ client/src/components/featured.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import productImg from '../images/productImg.png';
 
 const Featured = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_BASE_URL}/api/ecommerce/products`)
+      .then((res) => {
+        const list = Array.isArray(res.data) ? res.data : [];
+        setProducts(list.slice(0, 5));
+      })
+      .catch(() => setProducts([]));
+  }, []);
+
+const Featured = () => {
   return (
     <>
       <div id="gallery-head">
