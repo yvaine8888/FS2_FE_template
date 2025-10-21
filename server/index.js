6511320ffec487d95e49ee28499cd68bad9eeb8f
Yvaine Liew <yvaine8888@gmail.com>
2025-10-21 17:20:32 -0500

    Did some things

--- server/index.js
+++ server/index.js
@@ -14,15 +14,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // ✅ Using mysql2 + dotenv
 // TODO: Configure this pool with your schema credentials from Lesson 9.
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'e-commerce',
 });
 
 // TODO: Implement /submit-form to handle form data and insert into your database
 app.post('/submit-form', (req, res) => {
-  res.status(501).json({ message: 'Not implemented yet' });
+  const { firstName, lastName, email, subject } = req.body;
+  const sqlInsert = "INSERT INTO contact_form VALUES (NULL, ?, ?, ?, ?)";
+
+  if (!firstName || !lastName || !email || !subject)
+  {
+    return res.status(400).json({
+      message: 'Please fill out the entire form before submitting.'
+    })
+  }
+  db.query(sqlInsert, [firstName, lastName, email, subject], (err, result) => {
+    console.log(err);
+  })
 });
 
 // Optional: quick health check
